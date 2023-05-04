daee0b4d4c97e073192a3e2bcab06695e0c89642
a.epova <epova.aa@gmail.com>
2023-05-04 16:28:18 +0300

    check yandex api work

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.scss";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener("DOMContentLoaded", () => {
+    deliveryVariationChange();
+});
 
-setupCounter(document.querySelector('#counter'))
+function deliveryVariationChange() {
+    const buttons = document.querySelectorAll("[data-delivery]");
+    const inputs = document.querySelectorAll("[data-input]");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            inputs.forEach(input => {
+                input.classList.remove("active");
+               if (input.dataset.input === button.dataset.delivery) {
+                   input.classList.add("active");
+               }
+            });
+        });
+    });
+}
